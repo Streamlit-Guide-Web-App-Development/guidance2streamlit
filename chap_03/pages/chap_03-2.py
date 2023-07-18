018398a0f108caad9ff06e4b5d7e30e8db9651a3
jaehyeok.park <131845553+Parking93@users.noreply.github.com>
2023-07-18 22:30:49 +0900

    Update chap_03-2.py

--- chap_03/pages/chap_03-2.py
+++ chap_03/pages/chap_03-2.py
@@ -6,6 +6,11 @@
 import streamlit as st
 from PIL import Image
 import numpy as np
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+img_path = os.path.join(current_dir, "data/example.jpg")
+
 
 st.title('3.2 이미지, 비디오, 오디오 출력')
 
@@ -13,11 +18,13 @@ st.title('3.2 이미지, 비디오, 오디오 출력')
 st.divider()
 # st.image 예시 1
 st.markdown('#### *st.image 예시 1*')
-image = Image.open("./data/example.jpg")
+img_path = os.path.join(current_dir, "data/example.jpg")
+image = Image.open(img_path)
 
 st.image(image, caption="image1")
 # st.image 예시 2
 st.markdown('#### *st.image 예시 2*')
+
 image = Image.open("./data/example.jpg")
 
 st.image(image, caption="width=100", width=100)
@@ -29,7 +36,8 @@ st.image(image, caption="example", width=200, use_column_width="auto")
 st.divider()
 # st.audio 예시 1
 st.markdown('#### *st.audio 예시 1*')
-audio_file = open("./data/example.mp3", "rb")
+aud_path = os.path.join(current_dir, "data/example.mp3")
+audio_file = open(aud_path, "rb")
 
 audio_bytes = audio_file.read()
 
@@ -50,6 +58,7 @@ st.audio(note_la, sample_rate=sample_rate)
 st.divider()
 # st.video 예시 1
 st.markdown('#### *st.video 예시 1*')
+vid_path = os.path.join(current_dir, "data/example.mp4")
 # 동영상 파일을 로컬 경로에서 재생하는 예시
 video_file = open("./data/example.mp4", "rb")
 video_bytes = video_file.read()
@@ -57,7 +66,8 @@ video_bytes = video_file.read()
 st.video(video_bytes)
 # st.video 예시 2
 st.markdown('#### *st.video 예시 2*')
-video_file = open("./data/example1.mp4", "rb")
+vid_path = os.path.join(current_dir, "data/example1.mp4")
+video_file = open(vid_path, "rb")
 video_bytes = video_file.read()
 
 st.video(video_bytes)
